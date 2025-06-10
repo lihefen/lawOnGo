7d8141eea5653a8a7444fc8b009d9ab0ad2a6f25
lihe <lihe@jiayinfintech.cn>
2025-06-10 20:30:26 +0800

    law

--- services/sendCode.js
+++ services/sendCode.js
@@ -1,10 +1,11 @@
 
 import { useNuxtApp } from '#app';
+import lawUrl  from '~/utils/lawUrl';
 
 export const sendCode = ({ ...other } = {}, config = {}) => {
     const proxyDev =  process.env.NODE_ENV === 'development' ? '/proxyDev': '';
     const { $request } = useNuxtApp();
-    return $request(`${proxyDev}/api/app/login/send-code`, {
+    return $request(`${lawUrl}${proxyDev}/api/app/login/send-code`, {
 		data: {},
 		method: 'POST',
 		showToast: false,
---
+++ utils/lawUrl.js
@@ -0,0 +1,5 @@
+
+
+const lawUrl =  process.env.NODE_ENV === 'development' ? window.location.origin: 'https://app.lawongo.id';
+
+export  default lawUrl
