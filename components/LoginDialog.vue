c8e304b74bfcac6abb871a220f7e7b2be9ca26ab
lihe <lihe@jiayinfintech.cn>
2025-06-17 14:28:54 +0800

    登陆样式修改

--- components/LoginDialog.vue
+++ components/LoginDialog.vue
@@ -4,7 +4,7 @@
  * @Author: hean
  * @Date: 2025-05-10 15:14:40
  * @LastEditors: hean
- * @LastEditTime: 2025-05-13 00:15:26
+ * @LastEditTime: 2025-06-17 14:27:56
 -->
 <template>
    
@@ -23,64 +23,65 @@
             
             <section>
                 <div id="wrap-loginComponent">
-                    <div id="register" class="w-screen h-screen rounded-t-3xl bg-white md:rounded-3xl md:h-auto md:w-[465px] md:py-4 md:shadow-md">
-                        <div class="px-6 lg:px-8 pt-8 hidden md:block"><div class="flex justify-between"><p class="text-black pl-24 font-bold antialiased text-2xl"> Masuk Akun LawOnGo </p>
-                        <button @click="hide">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
-                        </button></div></div>
+                    <div id="register" class="w-screen h-screen rounded-t-3xl bg-[linear-gradient(180deg,#D7FFEE_0%,#FFFFFF_20%,#FFFFFF_100%)] md:rounded-3xl md:h-auto md:w-[465px] md:py-4 md:shadow-md">
+                        <div class="px-6 lg:px-8  hidden md:block">
+                            <div class="relative text-center pt-8">
+                                <p class="text-black font-bold antialiased text-2xl">Login LawOnGo Accoun</p>
+                                <button @click="hide" class="absolute right-0 top-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                </button>
+                                <img src="/image/login_icon.png" class="absolute left-0 top-0" alt="">
+                            </div>
+                        </div>
                         <div class="md:px-8 px-6">
-                            <div class="pt-8 lg:pt-10" v-if="!showCode">
-                                <p class="font-medium font-dm-sans antialiased mb-2 text-black text-base">Mobile Number</p>
-                                <div class="relative w-full ">
-                                    <el-input v-model="inputPhone" type="text" size="large" placeholder="08313302938***" ></el-input>
-                                </div>
-                                <button class="mt-4 py-2 w-full bg-[#04A45E] text-[#ffffff] text-bold text-lg rounded-md " @click="sendSms"> List </button>
-                            </div>
-
-                            <div class="space-y-6" v-if="showCode">
-                                <p class="text-2xl text-neutral-800 text-center font-bold" > Masukkan Kode OTP </p>
-                                <div class="flex items-center gap-2">
-                                    <img src="/image/icon-whatsapp-otp.svg" alt="icon-Whatsapp" class="w-12 h-12">
-                                    <p class="text-sm flex-1"> Kami telah mengirimkan 6 angka kode OTP melalui Whatsapp Anda </p>
-                                </div>
-                                <div class="w-fit bg-[#F3FBFF] rounded-lg px-3 py-1 text-sm text-center mx-auto">{{ inputPhone }}</div>
-                                <div class="flex justify-center">
-                                    <section>
-                                        <div>
-                                            <div>
-                                                <div class="relative w-full flex">
-                                                    <el-input
-                                                        v-model="inputCode"
-                                                        type="text"
-                                                        placeholder=""
-                                                        size="large" 
-                                                    />
-                                                </div>
-                                            </div>
+                                    <div class="pt-8 lg:pt-10" v-if="!showCode">
+                                        <p class="font-medium font-dm-sans antialiased mb-2 text-black text-base">Mobile Number</p>
+                                        <div class="relative w-full ">
+                                            <el-input v-model="inputPhone" type="text" size="large" placeholder="08313302938***" ></el-input>
                                         </div>
-                                    </section>
+                                        <button class="mt-4 py-2 w-full bg-[#04A45E] text-[#ffffff] text-bold text-lg rounded-md " @click="sendSms"> List </button>
+                                    </div>
+
+                                    <div class="space-y-6" v-if="showCode">
+                                        <p class="text-2xl text-neutral-800 text-center font-bold" > Masukkan Kode OTP </p>
+                                        <div class="flex items-center gap-2">
+                                            <img src="/image/icon-whatsapp-otp.svg" alt="icon-Whatsapp" class="w-12 h-12">
+                                            <p class="text-sm flex-1"> Kami telah mengirimkan 6 angka kode OTP melalui Whatsapp Anda </p>
+                                        </div>
+                                        <div class="w-fit bg-[#F3FBFF] rounded-lg px-3 py-1 text-sm text-center mx-auto">{{ inputPhone }}</div>
+                                        <div class="flex justify-center">
+                                            <section>
+                                                <div>
+                                                    <div>
+                                                        <div class="relative w-full flex">
+                                                            <el-input
+                                                                v-model="inputCode"
+                                                                type="text"
+                                                                placeholder=""
+                                                                size="large" 
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                        </div>
+                                        <button  @click="loginRequest" type="button" class="focus:outline-none focus:outline-0 focus-visible:outline-0 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400 flex-shrink-0 transition-all ease-in-out duration-300 w-full flex justify-center items-center px-4.5 py-2.5 md:text-lg bg-[#04A45E] text-white hover:bg-[#04A45E]/80 rounded-lg font-medium"><span> Login </span></button>
+                                    </div>
+                                
+                                    <div class="mt-6 px-3 py-2 lg:px-2 w-full">
+                                        <p class="text-black text-center text-base"> Don’t have a LawOnGo account yet? <span class="text-[#04A45E] lg:contents"><button @click="goRegister">Register here</button></span></p>
+                                    </div>
+                                    <div id="border-line" class="my-4"><div class="border border-[#CED1D6]"></div>
                                 </div>
-                                <button  @click="loginRequest" type="button" class="focus:outline-none focus:outline-0 focus-visible:outline-0 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400 flex-shrink-0 transition-all ease-in-out duration-300 w-full flex justify-center items-center px-4.5 py-2.5 md:text-lg bg-[#04A45E] text-white hover:bg-[#04A45E]/80 rounded-lg font-medium"><span> Login </span></button>
-                            </div>
-                            <!-- <div class="mt-6">
-                                <div>
-                                    <p class="font-medium font-dm-sans antialiased mb-2 text-black text-base">verification code</p>
-                                    <div class="relative w-full flex">
-                                        <el-input
-                                            v-model="inputCode"
-                                            type="text"
-                                            placeholder=""
-                                            size="large" 
-                                        />
-                                        <el-button class="ml-2" color="#04A45E" type="primary" size="large" @click="sendSms">send</el-button>
+                                <div id="login-advokat">
+                                    <p class="antialiased font-bold mb-2"> Are you a LawOnGo Advocate? </p><a href="/login/advokat" class=""><img class="w-full cursor-pointer" src="/image/lawyer/login/banner-login-advokat.png" alt="banner-advokat"></a>
+                                </div>
+                                <div class="pb-8 pt-4">
+                                    <div class="flex items-start gap-1">
+                                        <img class="w-3 mt-0.5" src="/image/info-icon.svg">
+                                        <p class="text-xs text-slate-500 antialiased"> Untuk pengalaman lebih baik, gunakan browser <span class="font-semibold">Google Chrome</span> atau <span class="font-semibold">Firefox</span></p>
                                     </div>
                                 </div>
-                            </div> -->
-                            <!-- <div class="mt-1.5 flex justify-end"><button class="text-sm text-[#04A45E] text-right cursor-pointer"> Forgot your password? </button></div> -->
-                            <div class="mt-6 px-3 py-2 lg:px-2 w-full"><p class="text-black text-center text-base"> Don’t have a LawOnGo account yet? <span class="text-[#04A45E] lg:contents"><button @click="goRegister">Register here</button></span></p></div>
-                            <div id="border-line" class="my-4"><div class="border border-[#CED1D6]"></div></div>
-                            <div id="login-advokat"><p class="antialiased font-bold mb-2"> Anda Advokat LawOnGo? </p><a href="/login/advokat" class=""><img class="w-full cursor-pointer" src="/image/lawyer/login/banner-login-advokat.png" alt="banner-advokat"></a></div>
-                            <div class="pb-8 pt-4"><div class="flex items-start gap-1"><img class="w-3 mt-0.5" src="/image/info-icon.svg"><p class="text-xs text-slate-500 antialiased"> Untuk pengalaman lebih baik, gunakan browser <span class="font-semibold">Google Chrome</span> atau <span class="font-semibold">Firefox</span></p></div></div>
                         </div>
                     </div>
                 </div>
